d27e3fa141ea0fef5856eb1959778cd636a0d91e
AlexAyuso01 <alejandro.ayusoex@icloud.com>
2022-04-04 13:06:28 +0200

    ESCCAR LEECAR  INI_BUFS y principio de PRINT

--- es_int.s
+++ es_int.s
@@ -7,3 +7,32 @@
         ORG $400
 
 **************************
+**************************** PRINT ********************************************
+** Escribe en un bufer interno (de tamaño 2000) de manera no bloqueante (acaba cuando termina de escribir Buffer)
+* Llama a ESCCAR
+* Devuelve el numero de caracteres copiados en D0
+* puntero de pila tiene que estar igual que al principio (LINK u ULINK)
+                    ****** RECUERDA ****** 
+                        *   An -> REGISTRO DE DIRECCIONES 
+                        *   Dn -> REGISTRO DE DATOS
+
+PRINT:
+                    LINK A6,#-36
+                    MOVEM.L	A0-A5/D1-D3,-(A6)
+                    ** RESET DE PARAMETROS Y LECTURA DE PARAMETROS(BUFFER(ireccion) 8,DESCRIPTOR(Dato) 12,TAMAÑO(dato) 16)**
+                    CLR     D0  
+                    CLR     D1
+                    CLR     D2
+
+                    **SELECCION DE BUFFER**
+
+                    **ERROR EN CARACTER**
+
+                    **INTERRUPCIONES**
+
+                    **FIN PRINT** 
+
+                    MOVEM.L	(A6)+,A0-A5/D1-D3                    
+                    UNLK A6
+
+
