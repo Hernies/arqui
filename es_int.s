adc6d524a268580c26cf0494409470e658d147f8
AlexAyuso01 <alejandro.ayusoex@icloud.com>
2022-04-07 20:14:46 +0200

    va bien

--- es_int.s
+++ es_int.s
@@ -301,13 +301,13 @@ PRINT:
                         **ESCRITURA**
         PA:             CMP.L       #0,D3           * SI SE HA ESCRITO TODO -> FIN
                         BEQ         FINP
-                        MOVE        D1,D4
+                        MOVE        D0,D4
                         MOVE        (A1)+,D1        * COPIAMOS EN D1 EL BUFFER
                         MOVE.L      #2,D0           * ESCCAR ESRIBA POR LTA
                         BSR         ESCCAR 
                         CMP.L       #$FFFFFFFF,D0   * MIRAMOS SI ESCCAR HA FALLADO SI?-> FIN
                         BEQ         FINP 
-                        MOVE.L      D4,D1    
+                        MOVE.L      D4,D0    
                         SUB.L       #1,D3
                         ADD.L       #1,D5           * CONTADOR++
                         BRA         PA
@@ -346,7 +346,7 @@ PRINT:
                         MOVE.B      D4,IMR 
                         BRA         FN_PRNT
                         **FIN PRINT** 
-        FN_PRNT:        MOVE.L D3,D0
+        FN_PRNT:        MOVE.L D5,D0
                         MOVEM.L	(A6)+,A0-A5/D1-D5                    
                         UNLK A6
                         RTS
