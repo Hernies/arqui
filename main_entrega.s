9e25e8f8356c3df0326da6c12b7bfe8a98982e3c
Hernies <hernancalvoaguiar.2@gmail.com>
2022-04-07 17:51:19 +0200

    el main para las entregas

--- main_entrega.s
+++ main_entrega.s
@@ -1,3 +1,14 @@
+
+BUFFER:  DS.B 2100 * Buffer para lectura y escritura de caracteres
+PARDIR:  DC.L 0 * Direcci´on que se pasa como par´ametro
+PARTAM:  DC.W 0 * Tama~no que se pasa como par´ametro
+CONTC:   DC.W 0 * Contador de caracteres a imprimir
+DESA:    EQU 0 * Descriptor l´ınea A
+DESB:    EQU 1 * Descriptor l´ınea B
+TAMBS:   EQU 30 * Tama~no de bloque para SCAN
+TAMBP:   EQU 7 * Tama~no de bloque para PRINT
+
+
 * Manejadores de excepciones
 INICIO: MOVE.L #BUS_ERROR,8 * Bus error handler
         MOVE.L #ADDRESS_ER,12 * Address error handler
